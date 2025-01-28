9bb624bb48a8632bb2e006dde9f952ab90d80a4c
Emanuel Inacio <emanuelinacio1@hotmail.com>
2025-01-27 23:05:48 -0700

    changes test assertions to use stretchr/testify/assert package

--- http/users_test.go
+++ http/users_test.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"net/http"
 	"net/http/httptest"
-	"reflect"
 	"testing"
 
 	"github.com/emanuelquerty/multency/domain"
@@ -31,9 +30,8 @@ func TestGetUser(t *testing.T) {
 		req := httptest.NewRequest("GET", "/api/users/1", nil)
 		res := newUserRequest(userStore, req)
 
-		if got, want := res.Code, 200; got != want {
-			t.Errorf("got %+v, want %+v", got, want)
-		}
+		got, want := res.Code, 200
+		assert.Equal(t, want, got, "status codes should be equal")
 	})
 
 	t.Run("returns user with id 7", func(t *testing.T) {
@@ -56,10 +54,7 @@ func TestGetUser(t *testing.T) {
 			Lastname:  "Petrelli",
 			Role:      "admin",
 		}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %+v, want %+v", got, want)
-		}
+		assert.Equal(t, want, got, "users should be equal")
 	})
 	t.Run("returns user with id 2", func(t *testing.T) {
 		user := domain.User{
@@ -86,10 +81,7 @@ func TestGetUser(t *testing.T) {
 			Lastname:  "Banner",
 			Role:      "trainer",
 		}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %+v, want %+v", got, want)
-		}
+		assert.Equal(t, want, got, "users should be equal")
 	})
 
 	t.Run("returns 404 status code for non-existing user id", func(t *testing.T) {
@@ -101,9 +93,8 @@ func TestGetUser(t *testing.T) {
 		req := httptest.NewRequest("GET", "/api/users/3", nil)
 		res := newUserRequest(userStore, req)
 
-		if got, want := res.Code, 404; got != want {
-			t.Errorf("got %d, want %d", got, want)
-		}
+		got, want := res.Code, 404
+		assert.Equal(t, want, got, "status codes should be equal")
 	})
 
 	t.Run("returns 400 status code for invalid user id", func(t *testing.T) {
@@ -115,9 +106,8 @@ func TestGetUser(t *testing.T) {
 		req := httptest.NewRequest("GET", "/api/users/notValidID3", nil)
 		res := newUserRequest(userStore, req)
 
-		if got, want := res.Code, 400; got != want {
-			t.Errorf("got %d, want %d", got, want)
-		}
+		got, want := res.Code, 400
+		assert.Equal(t, want, got, "status codes should be equal")
 	})
 
 }
@@ -144,9 +134,8 @@ func TestCreateUser(t *testing.T) {
 		req := httptest.NewRequest(http.MethodPost, "/api/users", bodyBuff)
 		res := newUserRequest(userStore, req)
 
-		if got, want := res.Code, 201; got != want {
-			t.Errorf("got %d, want %d", got, want)
-		}
+		got, want := res.Code, 201
+		assert.Equal(t, want, got, "status codes should be equal")
 	})
 
 	t.Run("returns newly created user", func(t *testing.T) {
@@ -164,9 +153,8 @@ func TestCreateUser(t *testing.T) {
 		var got domain.PublicUser
 		json.NewDecoder(res.Body).Decode(&got)
 
-		if want := MapToPublicUser(user); !reflect.DeepEqual(got, want) {
-			t.Errorf("got %+v, want %+v", got, want)
-		}
+		want := MapToPublicUser(user)
+		assert.Equal(t, want, got, "users should be equal")
 	})
 
 	t.Run("returns location header with full resource uri", func(t *testing.T) {
@@ -183,9 +171,7 @@ func TestCreateUser(t *testing.T) {
 
 		got := res.Header().Get("Location")
 		want := "://example.com/users/1" // newly created resource always has mocked ID = 1
-		if got != want {
-			t.Errorf("got %+v, want %+v", got, want)
-		}
+		assert.Equal(t, want, got, "urls should be equal")
 	})
 }
 
