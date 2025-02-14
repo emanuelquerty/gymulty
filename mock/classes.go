fce86e4e8d4fb1b55aa90a13e556086df3f3e748
Emanuel Inacio <emanuelinacio1@hotmail.com>
2025-02-13 21:25:19 -0700

    Adds compile time check for interface implementation

--- mock/classes.go
+++ mock/classes.go
@@ -6,6 +6,8 @@ import (
 	"github.com/emanuelquerty/gymulty/domain"
 )
 
+var _ domain.ClassStore = (*ClassStore)(nil)
+
 type ClassStore struct {
 	CreateClassFn func(ctx context.Context, tenantID int, class domain.Class) (domain.Class, error)
 }
